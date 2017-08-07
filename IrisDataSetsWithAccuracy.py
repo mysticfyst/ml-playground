6a33155f7c5e1a9d583b81c2549be52f08561b3d
mysticfyst <mysticfyst@gmail.com>
2017-08-07 09:22:31 +0530

    playing around with the code in ep4 of google dev ml tutorial

--- IrisDataSetsWithAccuracy.py
+++ IrisDataSetsWithAccuracy.py
@@ -1,5 +1,16 @@
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split as tp
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def accuracyScore(a, b):
+    sum = 0
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            sum += 1
+    return(sum/len(a))
+
 
 iris = datasets.load_iris()
 
@@ -7,4 +18,25 @@ x = iris.data
 y = iris.target
 
 xTrain, xTest, yTrain, yTest = tp(x, y, test_size=0.5)
-print(xTrain)
+
+dtClassifier = DecisionTreeClassifier()
+dtClassifier.fit(xTrain, yTrain)
+
+prediction1 = dtClassifier.predict(xTest)
+
+knnClassifier = KNeighborsClassifier()
+knnClassifier.fit(xTrain, yTrain)
+
+prediction2 = knnClassifier.predict(xTest)
+
+score1 = accuracyScore(prediction1, yTest)
+score2 = accuracyScore(prediction2, yTest)
+
+print(score1, score2, sep='  ')
+
+if score1 > score2:
+    print("Decision Tree performs better")
+elif score2 > score1:
+    print("KNN performs better")
+else:
+    print("Both the classifiers perform the same")
